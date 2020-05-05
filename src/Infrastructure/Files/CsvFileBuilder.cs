7f786583f5be344b2323445208d19e8f7ebd8f7d
Holovin <denys.holovin@cgi.com>
2020-05-05 15:02:32 -0300

    initial commit

---
+++ src/Infrastructure/Files/CsvFileBuilder.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Application.Common.Interfaces;
+using CleanArch.Application.TodoLists.Queries.ExportTodos;
+using CleanArch.Infrastructure.Files.Maps;
+using CsvHelper;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace CleanArch.Infrastructure.Files
+{
+    public class CsvFileBuilder : ICsvFileBuilder
+    {
+        public byte[] BuildTodoItemsFile(IEnumerable<TodoItemRecord> records)
+        {
+            using var memoryStream = new MemoryStream();
+            using (var streamWriter = new StreamWriter(memoryStream))
+            {
+                using var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
+
+                csvWriter.Configuration.RegisterClassMap<TodoItemRecordMap>();
+                csvWriter.WriteRecords(records);
+            }
+
+            return memoryStream.ToArray();
+        }
+    }
+}
